9299cb7dd1f6beae143d65528c5f94d95946bbaf
Xinyang <xinyzhang9@gmail.com>
2016-03-03 18:41:51 -0800

    insert sort list
    i think it’s not easy at all… bug prone

---
+++ insertion_sort_list.java
@@ -0,0 +1,34 @@
+// Given 1->3->2->0->null, return 0->1->2->3->null.
+/**
+ * Definition for ListNode.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */ 
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: The head of linked list.
+     */
+    public ListNode insertionSortList(ListNode head) {
+        // write your code here
+        ListNode dummy = new ListNode(0);
+        while(head!=null){
+            ListNode node = dummy;
+            while(node.next != null && node.next.val < head.val){
+                node = node.next;
+            }
+            ListNode tmp = head.next;
+            head.next = node.next;
+            node.next = head;
+            head = tmp;
+        }
+        return dummy.next;
+        
+    }
+}
