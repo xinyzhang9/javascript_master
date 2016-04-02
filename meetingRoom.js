036e9ad3ca9f8ee6f9598bb6aaf8eaba74a6ce36
Xinyang <xinyzhang9@gmail.com>
2016-04-02 01:46:28 -0700

    meeting room
    sort intervals in javascript

---
+++ meetingRoom.js
@@ -0,0 +1,27 @@
+// Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+
+// For example,
+// Given [[0, 30],[5, 10],[15, 20]],
+// return false.
+
+/**
+ * Definition for an interval.
+ * function Interval(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ * }
+ */
+/**
+ * @param {Interval[]} intervals
+ * @return {boolean}
+ */
+var canAttendMeetings = function(intervals) {
+    if(!intervals || intervals.length === 0)
+        return true;
+    intervals.sort(function(a,b){return a.start-b.start});
+    for(var i = 0; i < intervals.length-1; i++){
+        if(intervals[i].end > intervals[i+1].start)
+            return false;
+    }
+    return true;
+};
