f3baa7967e31bdf55512704ff326e8384e99d4a8
Xinyang <xinyzhang9@gmail.com>
2016-06-25 16:22:21 -0700

    mod chase me

--- rand_gen.js
+++ rand_gen.js
@@ -1,8 +1,8 @@
 var arr = [];
 var arr2 = [];
 for(var i = 0; i < 12; i++){
-	var r = Math.floor((Math.random() * 15) + 10);
-	var r2 = Math.floor((Math.random() * r) + 1);
+	var r = Math.floor((Math.random() * 8) + 16);
+	var r2 = Math.floor((Math.random() * (r-6)) + 6);
 	arr.push(r);
 	arr2.push(r2);
 }
